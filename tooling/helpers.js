42423ef34f96bdcc4e2fb6ccc39ebe0aa3bdcd25
moT01 <20648924+moT01@users.noreply.github.com>
2024-01-18 14:47:34 -0600

    fix: add helpers

--- tooling/helpers.js
+++ tooling/helpers.js
@@ -1,6 +1,20 @@
 import __helpers from '@freecodecamp/freecodecamp-os/.freeCodeCamp/tooling/test-utils.js';
+import { readFile, readdir } from 'fs/promises';
 import { Babeliser } from 'babeliser';
+import { join } from 'path';
+
+const ROOT = '/workspace/dotnet-curriculum'
 
 export async function babeliser(codeString) {
   return new Babeliser(codeString);
+}
+
+export async function getDirectory(path) {
+  const files = await readdir(join(ROOT, path));
+  return files;
+}
+
+export async function getFile(path) {
+  const file = await readFile(join(ROOT, path), 'utf8');
+  return file;
 }
